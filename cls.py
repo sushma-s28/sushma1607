c749da3b59134c2e49eff9e8900d47d7b014a5c7
sushma-s28 <sushmacse28@gmail.com>
2022-03-09 18:08:45 +0530

    cls.py

---
+++ cls.py
@@ -0,0 +1,29 @@
+import keyword
+# print(keyword.iskeyword("True"))
+# a=10
+# print(id(a))
+# print(dir(print))
+# a=10
+# b=20
+# print(a,sep="#",end="bye")
+# print(b,a)
+#
+# print(a,b,b)
+# value=input()
+# print("value is", value)
+a=10
+print(id(a))
+print(type(a))
+
+print(dir(a))
+
+a=30
+b=20
+print(a+b, a-b, a*b, a/b, a//b, a%b, a**b, divmod(a,b))
+c=1.2346
+print(round(c,3))
+import math
+a=4.56
+print(math.trunc(a))
+
+
